5092577618d3141985020e1d82a42d63d70a1bc1
Ankit mourya <mourya.ankit1612@gmail.com>
2026-05-11 02:01:24 +0530

    fix: add render url in axios.js and increase bottom margin of reviews in listing

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -6,8 +6,7 @@ import { getToken } from "../utils/token";
 
 const API = axios.create({
   // Use VITE_API_URL if it exists, otherwise default to local
-  // baseURL: import.meta.env.VITE_API_URL || "http://localhost:5000/api",
-  baseURL: "http://localhost:5000/api",
+  baseURL: import.meta.env.VITE_API_URL || "http://localhost:5000/api"
 });
 
 API.interceptors.request.use((req) => {
